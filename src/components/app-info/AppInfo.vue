a3be3658282f968d5d54c8f2da8c3aa83340e2cf
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-04 18:44:38 +0500

    Update AppInfo.vue

--- src/components/app-info/AppInfo.vue
+++ src/components/app-info/AppInfo.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <h1>App Info</h1>
+  <div class="app-info">
+    <p class="fs-3 text-uppercase">Barcha kinolar soni: 0</p>
+    <p class="fs-4 text-uppercase">Korilgan kinolar soni: 0</p>
   </div>
 </template>
 <script>
   export default {}
 </script>
 
-<style></style>
+<style scoped>
+.app-info{
+  padding: 1.5rem;
+  background: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0,0.15);
+}
+
+</style>
