66f4d0555bde66ec6c694c289a9c744f93062a8c
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-04 18:44:35 +0500

    Update AppFilter.vue

--- src/components/app-filter/AppFilter.vue
+++ src/components/app-filter/AppFilter.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <div>
-    <h1>App Filter</h1>
+  <div class="btn-group">
+    <button class="btn btn-dark" type="button">Barcha kinolar.</button>
+    <button class="btn btn-outline-dark" type="button">Mashxur kinolar.</button>
+    <button class="btn btn-outline-dark" type="button">Eng ko'p ko'rilgan kinolar.</button>
   </div>
 </template>
 
