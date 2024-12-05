d00cc484769e1eaa4050e1438d65f48ba327d743
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-05 09:46:56 +0500

    Update MovieAddForm.vue

--- src/components/movie-add-form/MovieAddForm.vue
+++ src/components/movie-add-form/MovieAddForm.vue
@@ -3,11 +3,22 @@
 </script>
 
 <template>
-  <div>
-    <h1>Movie Add Form</h1>
+  <div class="movie-add-form">
+    <h1>Yangi kino qo'shish</h1>
+    <form action="" class="add-form d-flex">
+      <input type="text" class="form-control new-movie-label" placeholder="Qanday kino ?">
+      <input type="text" class="form-control new-movie-label" placeholder="Necha marta ko'rilgan?">
+      <button class="btn btn-outline-dark" type="submit">Qo'shish</button>
+    </form>
   </div>
 </template>
 
 <style>
-  
+  .movie-add-form{
+    margin-top: 2rem;
+    padding: 1.5rem;
+    background: #fcfaf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0,0.15);
+  }
 </style>
