141cb368d85ad26dd81e911ee1c58274cce6564d
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-05 17:32:38 +0500

    Create BtnClick.vue

---
+++ src/components/btn-click/BtnClick.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="updateMessage">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 0,
+    };
+  },
+  methods: {
+    updateMessage() {
+      this.message += 1 ;
+    },
+  },
+};
+</script>
