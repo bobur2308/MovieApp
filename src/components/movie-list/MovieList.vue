dbc414c7d4289735f10eea2563f084857e7c94d1
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-17 17:12:19 +0500

    Update MovieList.vue

--- src/components/movie-list/MovieList.vue
+++ src/components/movie-list/MovieList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="movie-list list-group">
-    <MovieListItem v-for="movie in movies" :movie="movie" :key="movie.id" :onLike="$emit('onLike',movie.id)"/>
+    <MovieListItem v-for="movie in movies" :movie="movie" :key="movie.id" @onLike="$emit('onLike',movie.id)" @onCookie="$emit('onCookie',movie.id)" @onDelete="$emit('onDelete',movie.id)"/>
   </ul>
 </template>
 
